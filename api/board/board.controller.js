06911940727dd9bb53c61d403e6a2e830aa23e63
YtavPeer <ytav.peer@gmail.com>
2021-03-23 11:50:37 +0200

    first commit

---
+++ api/board/board.controller.js
@@ -0,0 +1,82 @@
+const logger = require('../../services/logger.service')
+const userService = require('../user/user.service')
+const socketService = require('../../services/socket.service')
+const boardService = require('./board.service')
+
+
+async function getBoards(req, res) {
+      try {
+            const boards = await boardService.query(req.query)
+            res.send(boards)
+      } catch (err) {
+            logger.error('Cannot get boards', err)
+            res.status(500).send({ err: 'Failed to get boards' })
+      }
+}
+
+async function getBoardById(req, res) {
+      try {
+            console.log('controller got req params,', req.params)
+            const { id } = req.params
+            // const { boardId } = req.params
+            console.log('controller got board id,', id)
+            board = await boardService.getById(id)
+            console.log('controller got board,', board)
+            res.json(board)
+      } catch (error) {
+            logger.error('Cannot get board', err)
+            res.status(500).send({ err: 'Failed to get board' })
+      }
+}
+
+async function addBoard(req, res) {
+      try {
+            var board = req.body
+            // board.byUserId = req.session.user._id
+            board = await boardService.add(board)
+
+            // // prepare the updated board for sending out
+            // board.byUser = await userService.getById(board.byUserId)
+            // board.aboutUser = await userService.getById(board.aboutUserId)
+
+            // console.log('CTRL SessionId:', req.sessionID);
+            // socketService.broadcast({ type: 'board-added', data: board })
+            // socketService.emitToAll({ type: 'user-updated', data: board.byUser, room: req.session.user._id })
+            res.send(board)
+
+      } catch (err) {
+            console.log(err)
+            logger.error('Failed to add board', err)
+            res.status(500).send({ err: 'Failed to add board' })
+      }
+}
+
+async function updateBoard(req, res) {
+      try {
+            const board = req.body
+            const updatedBoard = await boardService.update(board)
+            res.json(updatedBoard)
+      } catch (error) {
+            logger.error('Cannot update board', err)
+            res.status(500).send({ err: 'Failed to update board' })
+      }
+}
+
+async function deleteBoard(req, res) {
+      try {
+            await boardService.remove(req.params.id)
+            res.send({ msg: 'Deleted successfully' })
+      } catch (err) {
+            logger.error('Failed to delete board', err)
+            res.status(500).send({ err: 'Failed to delete board' })
+      }
+}
+
+
+module.exports = {
+      getBoards,
+      getBoardById,
+      addBoard,
+      updateBoard,
+      deleteBoard
+}
---
+++ api/board/board.service.js
@@ -0,0 +1,126 @@
+const dbService = require('../../services/db.service')
+const ObjectId = require('mongodb').ObjectId
+const asyncLocalStorage = require('../../services/als.service')
+
+async function query(filterBy = {}) {
+      try {
+            // const criteria = _buildCriteria(filterBy)
+            const collection = await dbService.getCollection('board')
+            const boards = await collection.find().toArray()
+            //   var boards = await collection.aggregate([
+            //       {
+            //           $match: filterBy
+            //       },
+            //       {
+            //           $lookup:
+            //           {
+            //               localField: 'byUserId',
+            //               from: 'user',
+            //               foreignField: '_id',
+            //               as: 'byUser'
+            //           }
+            //       },
+            //       {
+            //           $unwind: '$byUser'
+            //       },
+            //       {
+            //           $lookup:
+            //           {
+            //               localField: 'aboutUserId',
+            //               from: 'user',
+            //               foreignField: '_id',
+            //               as: 'aboutUser'
+            //           }
+            //       },
+            //       {
+            //           $unwind: '$aboutUser'
+            //       }
+            //   ]).toArray()
+            //   boards = boards.map(board => {
+            //       board.byUser = { _id: board.byUser._id, fullname: board.byUser.fullname }
+            //       board.aboutUser = { _id: board.aboutUser._id, fullname: board.aboutUser.fullname }
+            //       delete board.byUserId
+            //       delete board.aboutUserId
+            //       return board
+            //   })
+
+            return boards
+      } catch (err) {
+            logger.error('cannot find boards', err)
+            throw err
+      }
+
+}
+
+async function getById(boardId) {
+      try {
+            const collection = await dbService.getCollection('board')
+            const board = await collection.findOne({ '_id': ObjectId(boardId) })
+            // console.log('**** About toy ****', toy, toyId)
+            return board
+      } catch (error) {
+            logger.error(`while finding board ${boardId}`, err)
+            throw err
+      }
+}
+
+async function add(board) {
+      try {
+            // peek only updatable fields!
+            //     const boardToAdd = {
+            //         byUserId: ObjectId(board.byUserId),
+            //         aboutUserId: ObjectId(board.aboutUserId),
+            //         txt: board.txt
+            //     }
+            const collection = await dbService.getCollection('board')
+            await collection.insertOne(board)
+            return board;
+      } catch (err) {
+            logger.error('cannot insert board', err)
+            throw err
+      }
+}
+
+async function update(board) {
+      try {
+            board._id = ObjectId(board._id)
+            const collection = await dbService.getCollection('board')
+            await collection.updateOne({ '_id': board._id }, { $set: board })
+            console.log('updating board...')
+            return board;
+      } catch (error) {
+            logger.error(`cannot update board ${board._id}`, err)
+            throw err
+      }
+}
+
+async function remove(boardId) {
+      try {
+            //   const store = asyncLocalStorage.getStore()
+            //   const { userId, isAdmin } = store
+            const collection = await dbService.getCollection('board')
+            // remove only if user is owner/admin
+            const query = { _id: ObjectId(boardId) }
+            //   if (!isAdmin) query.byUserId = ObjectId(userId)
+            await collection.deleteOne(query)
+            // return await collection.deleteOne({ _id: ObjectId(boardId), byUserId: ObjectId(userId) })
+      } catch (err) {
+            logger.error(`cannot remove board ${boardId}`, err)
+            throw err
+      }
+}
+
+function _buildCriteria(filterBy) {
+      const criteria = {}
+      return criteria
+}
+
+module.exports = {
+      query,
+      getById,
+      add,
+      update,
+      remove,
+}
+
+
